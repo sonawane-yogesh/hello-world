20a5849c9c78e3d96985bf7f63d9a85c616ad0a6
sonawane-yogesh <sonawaneyogeshb@gmail.com>
2020-04-25 19:33:40 +0530

    after-gpg

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log("sonawane-yogesh@github.com");
 console.log("branch: yogeshs");
 // from branch sonawane
 console.log("branch: sonawane");
+
+// from yogeshs branch
+console.log("branch: yogeshs. Later after GPG Keys addition.");
